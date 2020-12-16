f4a2c138b2a8c031d697bb60410bf9e06d144609
unknown <darine.benmekki@esprit.tn>
2020-12-16 10:40:07 +0100

    My commit

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,88 +1,131 @@
+import io.cucumber.java.After;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+
 import org.testng.Assert;
 public class test1 {
 
-    public static void main (String[] args){
+
+
+    public static void main (String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
         WebDriver obj = new ChromeDriver();
         obj.get ("https://www.aodocs.com");
 
-        //Resize current window to the set dimension
-        //obj.manage().window().maximize();
 
         //click request a demo button
+
+
         obj.findElement(By.xpath("//*[@id=\"DND_banner-module-1\"]/div/div/div[1]/div/div/a")).click();
 
         //enter first name
-        obj.findElement(By.id("firstname-384ed391-59a1-4016-bc91-62bb1307edb2_3031")).sendKeys("Darine");
+
+        TimeUnit.SECONDS.sleep(10);
+
+        obj.findElement(By.name("firstname")).sendKeys("Darine");
 
         //Make random email
         String RandomEmail = UUID.randomUUID().toString();
 
         //Now this uuid enter to your text box
-        obj.findElement(By.id("email-384ed391-59a1-4016-bc91-62bb1307edb2_3031")).sendKeys(RandomEmail);
+        TimeUnit.SECONDS.sleep(10);
+        obj.findElement(By.name("email")).sendKeys(RandomEmail);
 
 
-        WebDriverWait wait = new WebDriverWait(obj, 15);
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@name='company_size__c']")));
 
 
         //Open list
-        obj.findElement(By.xpath("//select[@name='company_size__c']")).click();
+        TimeUnit.SECONDS.sleep(15);
+
+        obj.findElement(By.name("company_size__c")).click();
 
         //Choose campany size
-        obj.findElement(By.xpath("//option[contains(text(),'0-4 employees')]")).click();
+        TimeUnit.SECONDS.sleep(10);
+
+        obj.findElement(By.xpath("//*[@id=\"company_size__c-384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/option[2]")).click();
 
 
 
 
 
         //Click submit
-        obj.findElement(By.xpath("//input[@class='hs-button primary large']")).click();
+        //e can use also  (WebDriverWait wait = new WebDriverWait(obj, 15);)
+        TimeUnit.SECONDS.sleep(10);
+        obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/div/div[2]/input")).sendKeys(Keys.ENTER);
 
-        //Check error messages
+        //To Check error messages :
+
         //Email error msg
-        //String EmailError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/fieldset[2]/div[1]/ul/li/label")).getText();
-        //Assert.assertEquals("Email must be formatted correctly.", EmailError);
+        TimeUnit.SECONDS.sleep(10);
+        WebElement errMsgElem = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[2]/div[1]/ul/li/label"));
+        String errMessage = errMsgElem.getAttribute("innerText");
+        String expected_error="Email must be formatted correctly.";
+        Assert.assertEquals(errMessage,expected_error);
+        System.out.println("Email errors are checked ");
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[@class='hs-error-msg' and contains(@data-reactid,'lastname')]")));
-        //Last name error msg
-        String LastNameError = obj.findElement(By.xpath("//label[@class='hs-error-msg' and contains(@data-reactid,'lastname')]")).getText();
-        Assert.assertEquals("Please complete this required field.", LastNameError);
-
-
-
-        //check company msg
-        //String CompanyError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/fieldset[3]/div[1]/ul/li/label")).getText();
-        //Assert.assertEquals("Please complete this required field.", CompanyError);
-
-        //check country msg
-        //String CountryError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/fieldset[4]/div/ul/li/label")).getText();
-        //Assert.assertEquals("Please select an option from the dropdown menu.", CountryError);
-
-        //check msg field
-        //String MsgError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/fieldset[6]/div/ul/li/label")).getText();
-        //Assert.assertEquals("Please complete this required field.", MsgError);
-
-        //ho did you hear about us
-        //String HowDidYouError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/fieldset[7]/div/ul/li/label")).getText();
-        //Assert.assertEquals("Please select an option from the dropdown menu.", HowDidYouError);
-
-        //Complete form
-        //String CompleteFormError = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_3031\"]/div[1]/ul/li/label")).getText();
-        //Assert.assertEquals("Please complete all required fields.", CompleteFormError);
+           //Last name msg error
+        TimeUnit.SECONDS.sleep(10);
+        WebElement errMsgLastName = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[1]/div[3]/ul/li/label"));
+        String errMesg = errMsgElem.getAttribute("innerText");
 
 
-        obj.quit();
+        String expected_errorLastname="Please complete this required field.";
+        Assert.assertEquals(errMesg,expected_error);
+        System.out.println("last name error is checked");
+
+
+
+        //check company msg error
+
+        TimeUnit.SECONDS.sleep(5);
+        WebElement errCompanyName = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[3]/div[1]/ul/li/label"));
+        String errCompanyMSG = errMsgElem.getAttribute("innerText");
+
+
+        String expected_errorCompanyName="Please complete this required field.";
+        Assert.assertEquals(errCompanyMSG,expected_error);
+        System.out.println("Company name error is checked");
+
+        //check country error msg
+        TimeUnit.SECONDS.sleep(5);
+        WebElement errCountryName = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[4]/div/ul/li/label"));
+        String errCountryMSG = errMsgElem.getAttribute("innerText");
+        String expected_errorCountryName="Please select an option from the dropdown menu.";
+        Assert.assertEquals(errCountryMSG,expected_error);
+        System.out.println("Country selection error is checked");
+
+
+        //check msg field error
+        TimeUnit.SECONDS.sleep(5);
+        WebElement errMSGFIELD = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[6]/div/ul/li/label"));
+        String errMSG = errMsgElem.getAttribute("innerText");
+        String expected_errorMSGFIELD="Please complete this required field.";
+        Assert.assertEquals(errMSG,expected_error);
+        System.out.println("MSG FIELD error is checked");
+
+
+        //how did you hear about us  eroor msg
+
+        TimeUnit.SECONDS.sleep(5);
+
+        WebElement errSlection = obj.findElement(By.xpath("//*[@id=\"hsForm_384ed391-59a1-4016-bc91-62bb1307edb2_8038\"]/fieldset[7]/div/ul/li/label"));
+        String errHDYHAU = errMsgElem.getAttribute("innerText");
+        String expected_errSelection="Please select an option from the dropdown menu.";
+        Assert.assertEquals(errHDYHAU,expected_error);
+        System.out.println("HDYHAU error message is checked");
+
+      // Close the browser
+      obj.quit();
 
 
 
